008167ba0afb85a69ff45f43d0d7b8b369f58318
Henrique-TO <144277732+Henrique-TO@users.noreply.github.com>
2023-09-27 14:55:36 -0300

    Add files via upload

---
+++ aula_8.py
@@ -0,0 +1,20 @@
+frutas= ('abacaxi','banana','pitaya','morango','uva')
+print(frutas[2])
+
+n= (1,2,3,4,5,2,1,2,23,23,4,2)
+lista= list(n)
+print (lista)
+
+meses= ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro")
+for i in meses:
+    print(i)
+
+notas= (10,8,8,7,10,3)
+notas_tupla= tuple(notas)
+print(notas_tupla[0])
+
+coordenadas = (23,43,65)
+x,y,z=coordenadas
+print(f'Coordenada: {x}')
+print(f'Coordenada: {y}')
+print(f'Coordenada: {z}')
